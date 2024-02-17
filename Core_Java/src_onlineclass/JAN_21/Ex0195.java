95303c62a170753f7828ebb2994ff9e470475dd7
PAVITHRA D <dhanakotipavithra26@gmail.com>
2024-02-17 21:10:03 +0530

    Updating my core Java Program

---
+++ Core_Java/src_onlineclass/JAN_21/Ex0195.java
@@ -0,0 +1,21 @@
+package Core_Java.src_onlineclass.JAN_21;
+import java.util.Stack;
+public class Ex0195 {
+    public static void main(String[] args) {
+
+        // Stack
+        Stack<String> stack = new Stack();
+        stack.push("A");
+        stack.push("B");
+        stack.push("C");
+        //stack.push(123);
+        System.out.println(stack);
+        System.out.println(stack.pop());
+        System.out.println(stack);
+        System.out.println(stack.peek());
+        System.out.println(stack);
+        System.out.println(stack.pop());
+
+
+    }
+}
