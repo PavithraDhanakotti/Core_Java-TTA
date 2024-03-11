4e2b8d2d072a5a4adae467d81dcda7ce2940f7f3
PAVITHRA D <dhanakotipavithra26@gmail.com>
2024-03-11 20:27:03 +0530

    Updating my practice program

---
+++ Core_Java/src_practice/Lab0013.java
@@ -0,0 +1,20 @@
+package Core_Java.src_practice;
+
+public class Lab0013 {
+
+    // Generic
+    public static void main(String[] args) {
+        System.out.println(maxiandmin(21,56,99));
+    }
+
+    public static <DT extends Comparable <DT>> DT maxiandmin(DT a, DT b, DT c){
+        DT max =a ;
+        if ( b.compareTo(max)>0){
+            max=b;
+        }
+        if (c.compareTo(max)>0){
+            max=c;
+        }
+        return max;
+    }
+}
